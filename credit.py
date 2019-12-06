503da020ea64afc3a286e4aeb240e18927f2f3b6
Alekky09 <Alekky09@users.noreply.github.com>
2019-12-06 01:12:53 +0000

    automated commit by submit50

--- credit.py
+++ credit.py
@@ -8,15 +8,15 @@ while True:
         if c_num.isdigit():
             break
 
-    if len(c_num)!=13 and len(c_num)!=15 and len(c_num)!=16:
+    if len(c_num) != 13 and len(c_num) != 15 and len(c_num) != 16:
         print("INVALID")
         break
 
     nums = []
     calc = 0
 
-    for i in range(2, len(c_num)+1, 2):
-        nums.append(int(c_num[len(c_num)- i]))
+    for i in range(2, len(c_num) + 1, 2):
+        nums.append(int(c_num[len(c_num) - i]))
 
     for num in nums:
         num = num * 2
@@ -25,10 +25,10 @@ while True:
             num = int(new[0]) + int(new[1])
         calc = calc + num
 
-    for j in range(1, len(c_num)+1, 2):
-        calc = calc + int(c_num[len(c_num)- j])
+    for j in range(1, len(c_num) + 1, 2):
+        calc = calc + int(c_num[len(c_num) - j])
 
-    if (calc % 10)!=0:
+    if (calc % 10) != 0:
         print("INAVLID")
         break
     if c_num[0] == '3':
@@ -40,5 +40,3 @@ while True:
     if c_num[0] == '5':
         print("MASTERCARD")
         break
-
-
